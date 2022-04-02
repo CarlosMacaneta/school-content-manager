78290d6de42a6f84d60fea581546cb1eb5e0d06d
CarlosMacaneta <carlosmacaneta@hotmail.com>
2022-04-02 17:08:59 +0200

    firebase steup, gradle setup, some activities

---
+++ app/src/main/java/com/cs/schoolcontentmanager/ui/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.cs.schoolcontentmanager.ui.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cs.schoolcontentmanager.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
